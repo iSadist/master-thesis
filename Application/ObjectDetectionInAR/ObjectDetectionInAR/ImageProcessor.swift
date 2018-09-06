33a58c379a7d5cae1bb6e3bd799b568012651a8b
Jan Svensson <jansvensson@Jans-MacBook-Pro.local>
2018-09-06 12:17:19 +0200

    Save photos in ImageProcessor to photo app

--- Application/ObjectDetectionInAR/ObjectDetectionInAR/ImageProcessor.swift
+++ Application/ObjectDetectionInAR/ObjectDetectionInAR/ImageProcessor.swift
@@ -1,12 +1,22 @@
 import Foundation
+import UIKit
 import AVFoundation
 
 class ImageProcessor
 {
     var buffers: [CVPixelBuffer] = []
+    let croppedImages: [UIImage] = []
     
     func addBuffer(buffer: CVPixelBuffer)
     {
         buffers.append(buffer)
+    }
+    
+    func savePhotosToLibrary()
+    {
+        for croppedImage in croppedImages
+        {
+            UIImageWriteToSavedPhotosAlbum(croppedImage, nil, nil, nil)
+        }
     }
 }
