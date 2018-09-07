71b01e704c226342131be6492c73aea4f2d7142f
Jan Svensson <jansvensson@Jans-MacBook-Pro.local>
2018-09-07 14:21:35 +0200

    Add python script to generate artificial data
    From the taken images, this script will generate new images that are
    different and thus give more data to work with during training.
    
    This script only works with the images in the Seat folder at the moment.

---
+++ Training/artificialDataCreator.py
@@ -0,0 +1,26 @@
+from PIL import Image, ImageFilter, ImageEnhance
+
+for x in xrange(1863,2090):
+	image = Image.open('./Images/Seat/IMG_' + str(x) + '.jpg')
+	artificalImages = []
+
+	artificalImages.append(image.rotate(45))
+	artificalImages.append(image.rotate(90))
+	artificalImages.append(image.rotate(-45))
+	artificalImages.append(image.rotate(-90))
+	artificalImages.append(ImageEnhance.Brightness(image).enhance(0.8))
+	artificalImages.append(ImageEnhance.Brightness(image).enhance(1.2))
+	artificalImages.append(ImageEnhance.Contrast(image).enhance(1.4))
+	artificalImages.append(ImageEnhance.Contrast(image).enhance(0.7))
+	artificalImages.append(ImageEnhance.Color(image).enhance(0.5))
+	artificalImages.append(ImageEnhance.Color(image).enhance(1.6))
+	artificalImages.append(ImageEnhance.Sharpness(image).enhance(0.0))
+	artificalImages.append(ImageEnhance.Sharpness(image).enhance(2.0))
+
+	num = 1
+	for artificialImage in artificalImages:
+		artificialImage.save("./Images/ArtificialSeat/IMG_" + str(x) + "-" + str(num) + '.jpg', 'JPEG')
+		num += 1
+		pass
+
+	pass
