10130e9f73e69b7f2ae408e79353c7a720c9d2ec
Jan Svensson <jansvensson@Jans-MacBook-Pro.local>
2018-09-10 13:21:55 +0200

    Correctly add the pixel buffer to the ImageProcessor
    The previous pixel buffer was nil. So a workaround for this was done to get the
    data first, then at the end getting the pixel buffer from an CGImage.
    
    For now it works, but it should be changed in the future to work easier.

--- Application/ObjectDetectionInAR/ObjectDetectionInAR/ImageProcessor.swift
+++ Application/ObjectDetectionInAR/ObjectDetectionInAR/ImageProcessor.swift
@@ -12,6 +12,14 @@ class ImageProcessor
         buffers.append(buffer)
     }
     
+    func pixelBufferToUIImage(pixelBuffer: CVPixelBuffer) -> UIImage {
+        let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+        let context = CIContext(options: nil)
+        let cgImage = context.createCGImage(ciImage, from: ciImage.extent)
+        let uiImage = UIImage(cgImage: cgImage!)
+        return uiImage
+    }
+    
     func savePhotosToLibrary()
     {
         for croppedImage in croppedImages
--- Application/ObjectDetectionInAR/ObjectDetectionInAR/SamplePhotoViewController.swift
+++ Application/ObjectDetectionInAR/ObjectDetectionInAR/SamplePhotoViewController.swift
@@ -72,10 +72,21 @@ class SamplePhotoViewController: UIViewController, AVCapturePhotoCaptureDelegate
         // Save cropped images to a local storage
     }
     
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        guard let pixelBuffer = photo.pixelBuffer else { return }
-        imageProcessor.addBuffer(buffer: pixelBuffer)
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?)
+    {
+        let imageData = photo.fileDataRepresentation()
+        let image = UIImage(data: imageData!)
+        
+        // Converting UIImage to CGImage
+        let ciImage = CIImage(image: image!)
+        let context = CIContext(options: nil)
+        let cgImage = context.createCGImage(ciImage!, from: ciImage!.extent)
+        
+        // Get the pixel buffer
+        let pixelBuffer = convertToPixelBuffer(forImage: cgImage!)
+        imageProcessor.addBuffer(buffer: pixelBuffer!)
     }
+    
 
     override func didReceiveMemoryWarning()
     {
@@ -92,4 +103,30 @@ class SamplePhotoViewController: UIViewController, AVCapturePhotoCaptureDelegate
     }
     */
 
+    // MARK: Helper functions
+    
+    private func convertToPixelBuffer (forImage image:CGImage) -> CVPixelBuffer?
+    {
+        let frameSize = CGSize(width: image.width, height: image.height)
+        
+        var pixelBuffer:CVPixelBuffer? = nil
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(frameSize.width), Int(frameSize.height), kCVPixelFormatType_32BGRA , nil, &pixelBuffer)
+        
+        if status != kCVReturnSuccess
+        {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags.init(rawValue: 0))
+        let data = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue)
+        let context = CGContext(data: data, width: Int(frameSize.width), height: Int(frameSize.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: bitmapInfo.rawValue)
+        
+        context?.draw(image, in: CGRect(x: 0, y: 0, width: image.width, height: image.height))
+        
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBuffer
+    }
 }
