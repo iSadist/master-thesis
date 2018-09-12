5949a8744333232e1e55b04ac7848dd3daba53c7
Jan Svensson <jansvensson@Jans-MacBook-Pro.local>
2018-09-12 10:55:32 +0200

    Add testing part in trainer script and preventing overtraining
    Importing a new dataset for the model to test on and implementing
    dropout between layers for less overtraining.

--- Training/trainer.py
+++ Training/trainer.py
@@ -11,34 +11,35 @@ image_height = 300
 # fashion_mnist = keras.datasets.fashion_mnist
 # (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-def loadImageLibrary(library_name, label, start, end):
+def loadImageLibrary(library_name, image_list, label, label_list, start, end):
+	print("Saving images " + library_name + "...")
 	for num in xrange(start, end):
-		print("Saving image " + str(num) + "...")
 		for artificalNum in xrange(1,13):
 			filename = './Images/Artificial' + library_name + '/IMG_' + str(num) + '-' + str(artificalNum) + '.jpg'
-			addImage(filename, label)
+			addImage(filename, image_list, label, label_list)
 			pass
 		pass
-	pass
+	print("Done with " + library_name + "!")
 
-def addImage(filename, label):
+def addImage(filename, image_list, label, label_list):
 	image = Image.open(filename)
-	images.append(list(image.getdata(0)))
-	train_labels.append(label)
+	image_list.append(list(image.getdata(0)))
+	label_list.append(label)
 	return 
 
 # Create the neural network
 model = keras.Sequential([
-	keras.layers.Conv2D(16, kernel_size=(20,20), strides=(5, 5), input_shape=(image_width, image_height, 1)),
+	keras.layers.Conv2D(16, kernel_size=(5, 5), strides=(5, 5), input_shape=(image_width, image_height, 1)),
 	keras.layers.MaxPool2D(pool_size=(4, 4), padding="valid"),
 	keras.layers.Conv2D(16, kernel_size=(5,5), strides=(1, 1)),
 	keras.layers.MaxPool2D(pool_size=(4, 4), padding="valid"),
 	keras.layers.Flatten(),
     keras.layers.Dense(32, activation=tf.nn.relu),
+    keras.layers.Dropout(0.1),
     keras.layers.Dense(32, activation=tf.nn.relu),
+    keras.layers.Dropout(0.1),
     keras.layers.Dense(32, activation=tf.nn.relu),
-    keras.layers.Dense(32, activation=tf.nn.relu),
-    keras.layers.Dense(32, activation=tf.nn.relu),
+    keras.layers.Dropout(0.1),
     keras.layers.Dense(3, activation=tf.nn.softmax) # The number of nodes must be the same as the number of possibilities
 ])
 
@@ -49,38 +50,39 @@ model.summary()
 
 # Load images from datasets
 
-images = []
+train_images = []
 train_labels = []
 
-# Seats
-loadImageLibrary('Seat', 0, 1863, 1963)
+loadImageLibrary('Seat', train_images, 0, train_labels, 1863, 1963)
+loadImageLibrary('Piece1', train_images, 1, train_labels, 2253, 2353)
+loadImageLibrary('Piece2', train_images, 2, train_labels, 2536, 2636)
 
-print("Done with seats!")
-
-# Pieces 1
-loadImageLibrary('Piece1', 1, 2253, 2353)
-
-print("Done with pieces 1!")
-
-loadImageLibrary('Piece2', 2, 2536, 2636)
-
-print("Done with pieces 2!")
 print("Importing images complete!")
 print("--------------------------------")
 print("Reshaping to fit train images...")
 
-train_images = np.array(images).reshape(len(images), image_width, image_height, 1)
+train_images = np.array(train_images).reshape(len(train_images), image_width, image_height, 1)
 
 print("Reshaping complete!")
-
 print("Starting training...")
 
-model.fit(train_images, train_labels, epochs=200)
+model.fit(train_images, train_labels, epochs=10)
 
 print("Training complete!")
 
+print("Starting testing...")
+
+test_images = []
+test_labels = []
+
+loadImageLibrary('Seat', test_images, 0, test_labels, 1965, 2015)
+loadImageLibrary('Piece1', test_images, 1, test_labels, 2355, 2405)
+loadImageLibrary('Piece2', test_images, 2, test_labels, 2640, 2690)
+
+test_images = np.array(test_images).reshape(len(test_images), image_width, image_height, 1)
+
 # Evaluate the model
-# test_loss, test_acc = model.evaluate(test_images, test_labels)
-# print('Test accuracy:', test_acc)
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('Test accuracy:', test_acc)
 
 # model.save("recognizer.h5")
