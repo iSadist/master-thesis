26fdca1cf926fc4c74771247edb0b7269411750e
Jan Svensson <jansvensson@Jans-MacBook-Pro.local>
2018-09-14 16:30:01 +0200

    Remove old converters
    Previous commit added a new converter, thus this one is useless.

--- Training/Models/ml_converter.py
+++
@@ -1,5 +0,0 @@
-import tfcoreml as tf_converter
-tf_converter.convert(tf_model_path = 'output_graph.pb',
-                     mlmodel_path = 'furniture_parts.mlmodel',
-                     output_feature_names = ['k2tfout_0:0', 'k2tfout_1:0', 'k2tfout_2:0'],
-                     input_name_shape_dict = {'conv2d_input:0' : [1, 800, 600, 1]})
