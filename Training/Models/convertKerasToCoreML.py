6fc89dcb4a8f9058d7e5d4012af2f72ad5c42a6c
Jan Svensson <jansvensson@Jans-MacBook-Pro.local>
2018-09-14 16:30:59 +0200

    Change name on ml model after conversion

--- Training/Models/convertKerasToCoreML.py
+++ Training/Models/convertKerasToCoreML.py
@@ -5,4 +5,4 @@ coreml_model = coremltools.converters.keras.convert('recognizer.h5',
 	image_input_names="image",
 	image_scale=1/255.0,
 	is_bgr=False)
-coreml_model.save('my_model.mlmodel')
+coreml_model.save('Furniture.mlmodel')
