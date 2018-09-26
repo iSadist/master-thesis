5cb740ece1b0640a89f0e220beaada8766be5dd4
Jan Svensson <jansvensson@Jans-MacBook-Pro-2.local>
2018-09-26 15:26:07 +0200

    Fix wrong value on image scale
    The image should not scale since it trains on values between 0 and 255.

--- Training/Models/convertKerasToCoreML.py
+++ Training/Models/convertKerasToCoreML.py
@@ -3,6 +3,7 @@ import coremltools
 coreml_model = coremltools.converters.keras.convert('recognizer.h5',
 	input_names="image",
 	image_input_names="image",
-	image_scale=1/255.0,
-	is_bgr=False)
+	image_scale=1,
+	is_bgr=False,
+	class_labels = ['Unknown', 'Seat', 'Piece 1', "Piece 2"])
 coreml_model.save('Furniture.mlmodel')
