9f0dcf509cfd3a4d3a414f27fcf5cfef3d20a7e0
Jan Svensson <jansvensson@Jans-MacBook-Pro-2.local>
2018-09-28 18:41:50 +0200

    Implement Furniture inspector
    In the furniture inspector the user will be
    able to preview the furniture in a 3D scene.

---
+++ Application/ObjectDetectionInAR/ObjectDetectionInAR/FurnitureInspecterViewController.swift
@@ -0,0 +1,37 @@
+import UIKit
+import SceneKit
+
+class FurnitureInspecterViewController: UIViewController
+{
+    @IBOutlet weak var sceneView: SCNView!
+    var cameraConfiguration: SCNCameraControlConfiguration?
+    var modelNode: SCNNode?
+    var cameraNode: SCNNode?
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        modelNode = scene.rootNode.childNode(withName: "ship", recursively: false)?.childNodes.first
+        cameraNode = scene.rootNode.childNode(withName: "camera", recursively: false)
+        cameraNode?.constraints = [SCNLookAtConstraint(target: modelNode)]
+        sceneView.scene = scene
+    }
+
+    @IBAction func panGesture(_ sender: UIPanGestureRecognizer)
+    {
+        let velocity = sender.velocity(in: sceneView)
+        modelNode?.runAction(SCNAction.rotateBy(x: -velocity.y/5000, y: velocity.x/5000, z: 0, duration: 0.1))
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
