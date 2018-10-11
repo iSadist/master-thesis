c52f93fdbc315ff0f50657e605b9cdca600f5e69
Jan Svensson <jsvensson51@yahoo.com>
2018-10-11 14:16:10 +0200

    Make training configuration with 93% hitrate

--- Training/trainer.py
+++ Training/trainer.py
@@ -13,10 +13,11 @@ number_of_color_channels = 3
 
 training_share = 0.9
 
-number_of_total_images = 154
-training_batch_size = int(number_of_total_images * training_share)
-
 image_libraries = ["UnknownObjects", "Seat", "Piece1", "Piece2"]
+
+number_of_total_images = 154 
+training_batch_size = int(number_of_total_images * training_share * 18 * len(image_libraries))
+
 
 # MARK: - Functions
 
@@ -45,23 +46,22 @@ def reshapeArray(oldArray):
 def createModel():
 	# Create the neural network
 	model = keras.Sequential([
-		keras.layers.Conv2D(16, kernel_size=(10, 10), strides=(2, 2), input_shape=(image_height, image_width, number_of_color_channels)),
+		keras.layers.Conv2D(16, kernel_size=(3, 3), strides=(1, 1), input_shape=(image_height, image_width, number_of_color_channels)),
+		keras.layers.Conv2D(16, kernel_size=(2,2), strides=(1, 1)),
 		keras.layers.MaxPool2D(pool_size=(2, 2), padding="valid"),
-		keras.layers.Dropout(0.2),
-		keras.layers.Conv2D(16, kernel_size=(8,8), strides=(2, 2)),
+		keras.layers.Dropout(0.4),
+		keras.layers.Conv2D(16, kernel_size=(3,3), strides=(1, 1)),
+		keras.layers.Conv2D(16, kernel_size=(2,2), strides=(1, 1)),
 		keras.layers.MaxPool2D(pool_size=(2, 2), padding="valid"),
-		keras.layers.Dropout(0.2),
-		keras.layers.Conv2D(16, kernel_size=(4,4), strides=(1, 1)),
+		keras.layers.Dropout(0.4),
+		keras.layers.Conv2D(32, kernel_size=(3,3), strides=(1, 1)),
 		keras.layers.MaxPool2D(pool_size=(2, 2), padding="valid"),
-		keras.layers.Dropout(0.2),
+		keras.layers.Dropout(0.4),
 		keras.layers.Flatten(),
-	    keras.layers.Dense(32, activation=tf.nn.relu),
+	    keras.layers.Dense(128, activation=tf.nn.relu),
+	    keras.layers.Dropout(0.5),
+	    keras.layers.Dense(64, activation=tf.nn.relu),
 	    keras.layers.Dropout(0.2),
-	    keras.layers.Dense(32, activation=tf.nn.relu),
-	    keras.layers.Dropout(0.1),
-	    keras.layers.Dense(16, activation=tf.nn.relu),
-	    keras.layers.Dropout(0.1),
-	    keras.layers.Dense(16, activation=tf.nn.relu),
 	    keras.layers.Dense(4, activation=tf.nn.softmax) # The number of nodes must be the same as the number of possibilities
 	])
 
@@ -74,8 +74,8 @@ def createModel():
 def trainModel(model, train_data, train_labels):
 	print("Starting training...")
 	train_data_size = len(train_data)
-	validation_part = 0.5
-	partial_train_size = int(train_data_size*validation_part)
+	training_part = 0.75
+	partial_train_size = int(train_data_size*training_part)
 
 	partial_x_train = train_data[:partial_train_size]
 	partial_y_train = train_labels[:partial_train_size]
@@ -84,16 +84,12 @@ def trainModel(model, train_data, train_labels):
 	y_validation = train_labels[partial_train_size:]
 
 
-	history = model.fit(train_data, train_labels, epochs=150, batch_size=476, validation_data=(x_validation, y_validation), verbose=1)
+	history = model.fit(train_data, train_labels, epochs=100, batch_size=100, validation_data=(x_validation, y_validation), verbose=1)
 	return history
 
-def loadTrainImages(image_list, labels_list):
+def loadImages(image_list, labels_list):
 	for i in range(0,len(image_libraries)):
-		loadImageLibrary(image_libraries[i], image_list, i, labels_list, 1, training_batch_size)
-
-def loadTestImages(image_list, labels_list):
-	for i in range(0,len(image_libraries)):
-		loadImageLibrary(image_libraries[i], image_list, i, labels_list, training_batch_size + 1, number_of_total_images)
+		loadImageLibrary(image_libraries[i], image_list, i, labels_list, 1, number_of_total_images)
 
 def test(model, test_images,test_labels):
 	print("Start testing...")
@@ -101,21 +97,22 @@ def test(model, test_images,test_labels):
 	print('Test accuracy:', test_acc)
 
 def main():
-	train_images = []
-	train_labels = []
+	images = []
+	labels = []
 
-	test_images = []
-	test_labels = []
+	loadImages(images, labels)
+	images = reshapeArray(images)
+	images, labels = shuffle(images, labels)
 
-	loadTrainImages(train_images, train_labels)
-	train_images = reshapeArray(train_images)
-	train_images, train_labels = shuffle(train_images, train_labels)
+	train_images = images[:training_batch_size]
+	train_labels = labels[:training_batch_size]
+
+	test_images = images[training_batch_size + 1:]
+	test_labels = labels[training_batch_size + 1:]
 
 	model = createModel()
 	history = trainModel(model, train_images, train_labels)
 	model.save("./Models/recognizer.h5")
-	loadTestImages(test_images,test_labels)
-	test_images = reshapeArray(test_images)
 	test(model, test_images,test_labels)
 
 	plotter.graphTrainingData(history)
