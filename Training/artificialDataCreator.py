3a8c969ebc8c7d8f90a7a5712575df5b39db1edc
Jan Svensson <jsvensson51@yahoo.com>
2018-10-15 08:55:11 +0200

    Split Test and Training data
    This split makes it so that no augmented images from the train set
    are tested on. This is not good since it gives a false impression
    that the model is really good. The model has already seen the
    original images during training and the modified are thus easier
    to recognize.

--- Training/artificialDataCreator.py
+++ Training/artificialDataCreator.py
@@ -1,11 +1,15 @@
 from PIL import Image, ImageFilter, ImageEnhance
+from sklearn.utils import shuffle
 
 image_width = 200
 image_height = 150
 
-def create(name, number_of_images):
-	for x in range(1,number_of_images):
-		image = Image.open('./Images/' + name + '/image_' + str(x) + '.jpg')
+def create(name, folder, number_of_images):
+	image_numbers = range(1,number_of_images + 1)
+	image_numbers = shuffle(image_numbers)
+
+	for x in range(1, len(image_numbers) + 1):
+		image = Image.open('./Images/' + folder + '/' + name + '/image_' + str(image_numbers[x-1]) + '.jpg')
 		image = image.resize((image_width,image_height),Image.LANCZOS)
 
 		image_90 = image.rotate(90)
@@ -20,7 +24,7 @@ def create(name, number_of_images):
 		artificalImages.append(image.transpose(Image.FLIP_LEFT_RIGHT))
 		artificalImages.append(ImageEnhance.Brightness(image).enhance(0.7))
 		artificalImages.append(ImageEnhance.Brightness(image_90).enhance(1.3))
-		artificalImages.append(ImageEnhance.Brightness(image_270).enhance(0.4))
+		artificalImages.append(ImageEnhance.Brightness(image_270).enhance(0.7))
 		artificalImages.append(ImageEnhance.Brightness(image_180.transpose(Image.FLIP_LEFT_RIGHT)).enhance(1.5))
 		artificalImages.append(ImageEnhance.Contrast(image).enhance(1.4))
 		artificalImages.append(ImageEnhance.Contrast(image_180).enhance(0.7))
@@ -30,14 +34,18 @@ def create(name, number_of_images):
 		artificalImages.append(ImageEnhance.Sharpness(image).enhance(0.0))
 		artificalImages.append(ImageEnhance.Sharpness(image_180).enhance(2.0))
 		artificalImages.append(image.filter(ImageFilter.GaussianBlur(1)))
-		artificalImages.append(image_90.filter(ImageFilter.MinFilter(3)))
 
 		num = 1
 		for artificialImage in artificalImages:
-			artificialImage.save("./Images/Artificial" + name + "/image_" + str(x) + "-" + str(num) + '.jpg', 'JPEG')
+			artificialImage.save("./Images/" + folder + "/Artificial" + name + "/image_" + str(x) + "-" + str(num) + '.jpg', 'JPEG')
 			num += 1
 
-create('Seat', 154)
-create('Piece1', 154)
-create('Piece2', 154)
-create('UnknownObjects', 154)
+create('Seat', 'Train', 134)
+create('Piece1', 'Train', 134)
+create('Piece2', 'Train', 134)
+create('UnknownObjects', 'Train', 134)
+
+create('Seat', 'Test', 12)
+create('Piece1', 'Test', 12)
+create('Piece2', 'Test', 12)
+create('UnknownObjects', 'Test', 12)
--- Training/trainer.py
+++ Training/trainer.py
@@ -10,26 +10,16 @@ import plotter
 image_width = 200
 image_height = 150
 number_of_color_channels = 3
-
-training_share = 0.9
-
 image_libraries = ["UnknownObjects", "Seat", "Piece1", "Piece2"]
-
-number_of_total_images = 154 
-training_batch_size = int(number_of_total_images * training_share * 18 * len(image_libraries))
-
 
 # MARK: - Functions
 
-def loadImageLibrary(library_name, image_list, label, label_list, start, end):
+def loadImageLibrary(library_name, folder, image_list, label, label_list, start, end):
 	print("Saving images " + library_name + "...")
-	for num in range(start, end):
+	for num in range(start, end + 1):
 		for artificalNum in range(1,18):
-			filename = './Images/Artificial' + library_name + '/image_' + str(num) + '-' + str(artificalNum) + '.jpg'
+			filename = './Images/' + folder + '/Artificial' + library_name + '/image_' + str(num) + '-' + str(artificalNum) + '.jpg'
 			addImage(filename, image_list, label, label_list)
-			pass
-		pass
-	print("Done with " + library_name + "!")
 
 def addImage(filename, image_list, label, label_list):
 	image = Image.open(filename)
@@ -46,22 +36,23 @@ def reshapeArray(oldArray):
 def createModel():
 	# Create the neural network
 	model = keras.Sequential([
-		keras.layers.Conv2D(16, kernel_size=(3, 3), strides=(1, 1), input_shape=(image_height, image_width, number_of_color_channels)),
-		keras.layers.Conv2D(16, kernel_size=(2,2), strides=(1, 1)),
+		keras.layers.Conv2D(8, kernel_size=(3, 3), strides=(1, 1), input_shape=(image_height, image_width, number_of_color_channels)),
+		keras.layers.Conv2D(8, kernel_size=(2,2), strides=(1, 1)),
+		keras.layers.Conv2D(8, kernel_size=(3,3), strides=(1, 1)),
+		keras.layers.Conv2D(8, kernel_size=(2,2), strides=(1, 1)),
 		keras.layers.MaxPool2D(pool_size=(2, 2), padding="valid"),
-		keras.layers.Dropout(0.4),
-		keras.layers.Conv2D(16, kernel_size=(3,3), strides=(1, 1)),
-		keras.layers.Conv2D(16, kernel_size=(2,2), strides=(1, 1)),
+		keras.layers.Conv2D(8, kernel_size=(3,3), strides=(1, 1)),
+		keras.layers.Conv2D(8, kernel_size=(2,2), strides=(1, 1)),
 		keras.layers.MaxPool2D(pool_size=(2, 2), padding="valid"),
-		keras.layers.Dropout(0.4),
-		keras.layers.Conv2D(32, kernel_size=(3,3), strides=(1, 1)),
+		keras.layers.Conv2D(8, kernel_size=(3,3), strides=(1, 1)),
+		keras.layers.Conv2D(8, kernel_size=(2,2), strides=(1, 1)),
 		keras.layers.MaxPool2D(pool_size=(2, 2), padding="valid"),
-		keras.layers.Dropout(0.4),
+		keras.layers.Conv2D(8, kernel_size=(2,2), strides=(1, 1)),
+		keras.layers.MaxPool2D(pool_size=(4, 4), padding="valid"),
 		keras.layers.Flatten(),
+		keras.layers.Dropout(0.5),
 	    keras.layers.Dense(128, activation=tf.nn.relu),
 	    keras.layers.Dropout(0.5),
-	    keras.layers.Dense(64, activation=tf.nn.relu),
-	    keras.layers.Dropout(0.2),
 	    keras.layers.Dense(4, activation=tf.nn.softmax) # The number of nodes must be the same as the number of possibilities
 	])
 
@@ -71,25 +62,15 @@ def createModel():
 	model.summary()
 	return model
 
-def trainModel(model, train_data, train_labels):
+def trainModel(model, train_data, train_labels, test_images, test_labels):
 	print("Starting training...")
-	train_data_size = len(train_data)
-	training_part = 0.75
-	partial_train_size = int(train_data_size*training_part)
-
-	partial_x_train = train_data[:partial_train_size]
-	partial_y_train = train_labels[:partial_train_size]
-
-	x_validation = train_data[partial_train_size:]
-	y_validation = train_labels[partial_train_size:]
-
-
-	history = model.fit(train_data, train_labels, epochs=100, batch_size=100, validation_data=(x_validation, y_validation), verbose=1)
+	train_data, train_labels = shuffle(train_data, train_labels) # Shuffle the whole training set
+	history = model.fit(train_data, train_labels, epochs=100, batch_size=20, validation_data=(test_images, test_labels), verbose=1)
 	return history
 
-def loadImages(image_list, labels_list):
+def loadImages(image_list, labels_list, folder, number_of_images):
 	for i in range(0,len(image_libraries)):
-		loadImageLibrary(image_libraries[i], image_list, i, labels_list, 1, number_of_total_images)
+		loadImageLibrary(image_libraries[i], folder, image_list, i, labels_list, 1, number_of_images)
 
 def test(model, test_images,test_labels):
 	print("Start testing...")
@@ -97,23 +78,22 @@ def test(model, test_images,test_labels):
 	print('Test accuracy:', test_acc)
 
 def main():
-	images = []
-	labels = []
+	train_images = []
+	train_labels = []
+	loadImages(train_images, train_labels, "Train", 134)
+	# loadImages(train_images, train_labels, "Train", 10) 
+	train_images = reshapeArray(train_images)
 
-	loadImages(images, labels)
-	images = reshapeArray(images)
-	images, labels = shuffle(images, labels)
-
-	train_images = images[:training_batch_size]
-	train_labels = labels[:training_batch_size]
-
-	test_images = images[training_batch_size + 1:]
-	test_labels = labels[training_batch_size + 1:]
+	test_images = []
+	test_labels = []
+	loadImages(test_images, test_labels, "Test", 12)
+	# loadImages(test_images, test_labels, "Test", 3)
+	test_images = reshapeArray(test_images)
 
 	model = createModel()
-	history = trainModel(model, train_images, train_labels)
+	history = trainModel(model, train_images, train_labels, test_images, test_labels)
 	model.save("./Models/recognizer.h5")
-	test(model, test_images,test_labels)
+	test(model, test_images, test_labels)
 
 	plotter.graphTrainingData(history)
 
