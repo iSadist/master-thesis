286031b96dda558df4f211d471495c6c0ff5c28a
Jan Svensson <jsvensson51@yahoo.com>
2018-10-23 23:00:41 +0200

    Prevent user from setting up a furniture with no instructions

--- Application/ObjectDetectionInAR/ObjectDetectionInAR/FurnitureSelection/FurnitureDetailViewController.swift
+++ Application/ObjectDetectionInAR/ObjectDetectionInAR/FurnitureSelection/FurnitureDetailViewController.swift
@@ -5,16 +5,31 @@ class FurnitureDetailViewController: UIViewController
     @IBOutlet weak var iconView: UIImageView!
     @IBOutlet weak var idLabel: UILabel!
     @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var assembleButton: UIButton!
     
     var furniture: Furniture?
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
         iconView.image = furniture?.icon
         idLabel.text = furniture?.id
         descriptionTextView.text = furniture?.description
         self.navigationItem.title = furniture?.name
+    }
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        let database = Database()
+        furniture?.instructions = database.getInstructions(for: furniture!)
+        
+        // Don't allow the user to navigate further if no instructions exist
+        if furniture?.instructions == nil
+        {
+            assembleButton.isEnabled = false
+            assembleButton.setTitle("No available instructions", for: .normal)
+        }
     }
     
     // MARK: - Navigation
