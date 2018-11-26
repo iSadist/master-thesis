315cb6b68d75228bbc9c22172466c309728ecb2e
Jan Svensson <jsvensson51@yahoo.com>
2018-11-26 20:26:52 +0100

    Write about a positive feedback loop in object tracking

--- Application/ObjectDetectionInAR/ObjectDetectionInAR/Assembler/AssemblerViewController.swift
+++ Application/ObjectDetectionInAR/ObjectDetectionInAR/Assembler/AssemblerViewController.swift
@@ -327,6 +327,8 @@ extension AssemblerViewController: InstructionExecutionerDelegate
         let snapshot = sceneView.snapshot()
         let converter = ImageConverter()
         
+        sceneView.session.currentFrame?.capturedImage
+        
         return converter.convertImageToPixelBuffer(image: snapshot)
     }
 }
